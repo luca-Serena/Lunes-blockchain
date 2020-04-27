596551f00838ecfa58f378a24738dfdc063f5f2d
Luca <lucaserena35@gmail.com>
2020-04-28 01:08:36 +0200

    Removed generated txt files

--- selfishAttacks.sh
+++ selfishAttacks.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-zero="0"
 file='selfish-res.txt'
 if [ -f $file ] ; then
     rm $file
@@ -10,6 +9,9 @@ for i in outputs/*test-Selfish-*.txt ;do
     rate=$(echo "$i" | cut -d'-' -f 3 | cut -d'.' -f1)
     if [ "$rate" -lt 10 ] ; then
     	rate="0$rate"
+    fi
+    if [ "$rate" -lt 100 ] ; then
+        rate="0$rate"
     fi
     succed=$(cat $i | grep -c "possible")
 	succed=$(cat $i | tail -8 | head -1 | cut -d "," -f6)
